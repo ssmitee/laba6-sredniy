67f39cb1e25515ee18eaec282f0d90f3d57cb511
= <user28@mail.ru>
2024-05-04 14:20:50 +0200

    Добавьте файлы проекта.

---
+++ laba6 sredniy/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите вещественные числа (для остановки введите отрицательное число): ");
+
+        string input = Console.ReadLine();
+        string[] numbersStr = input.Split(' ');
+
+        double[] numbers = new double[numbersStr.Length];
+
+        for (int i = 0; i < numbersStr.Length; i++)
+        {
+            numbers[i] = double.Parse(numbersStr[i]);
+            if (numbers[i] < 0)
+            {
+                Array.Resize(ref numbers, i);
+                break;
+            }
+        }
+
+        if (numbers.Length == 0)
+        {
+            Console.WriteLine("Массив пустой, так как не было введено положительных чисел.");
+            return;
+        }
+
+        double min = numbers[0];
+        int indexOfMin = 0;
+
+        for (int i = 1; i < numbers.Length; i++)
+        {
+            if (numbers[i] < min)
+            {
+                min = numbers[i];
+                indexOfMin = i;
+            }
+        }
+
+        Console.WriteLine($"Индекс наименьшего элемента в массиве: {indexOfMin}");
+    }
+}
